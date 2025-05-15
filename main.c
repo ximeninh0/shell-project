e5386509e5318649f72dfdbdb2cd808fed30e854
Guilherme Andrade <guiandradeemail@gmail.com>
2025-05-15 11:46:44 -0300

    ✨ feat: execute command

--- main.c
+++ main.c
@@ -1,30 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define LSH_RL_BUFSIZE 1024
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 
-char *lsh_read_line(void)
-{
-    char *line = NULL;
-    ssize_t bufsize = 0;
+char *lsh_read_line(void);
+char **lsh_split_line(char *line);
+void header();
 
-    if (getline(&line, &bufsize, stdin) == -1)
+int main()
+{
+    char *line;
+    char **args;
+    int status;
+
+    header();
+
+    do
     {
-        if (feof(stdin))
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line); 
+
+        if (args[0] == NULL)
         {
-            exit(EXIT_SUCCESS);
+            free(line);
+            free(args);
+            continue;
+        }
+
+        if (strcmp(args[0], "exit") == 0)
+        {
+            free(line);
+            free(args);
+            break;
+        }
+
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            if (execvp(args[0], args) == -1)
+            {
+                perror("lsh");
+            }
+            exit(EXIT_FAILURE);
+        }
+        else if (pid < 0)
+        {
+            perror("lsh");
         }
         else
         {
-            perror("readline");
-            exit(EXIT_FAILURE);
+            waitpid(pid, &status, 0);
+            printf("\n");
         }
-    }
 
-    return line;
+        free(line);
+        free(args);
+
+    } while (1);
+
+    return 0;
+}
+
+void header() {
+    printf(" ██████╗██████╗  █████╗ ███████╗██╗  ██╗\n");
+    printf("██╔════╝██╔══██╗██╔══██╗██╔════╝██║  ██║\n");
+    printf("██║     ██████╔╝███████║███████╗███████║\n");
+    printf("██║     ██╔══██╗██╔══██║╚════██║██╔══██║\n");
+    printf("╚██████╗██║  ██║██║  ██║███████║██║  ██║\n");
+    printf(" ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝\n");
 }
 
 char **lsh_split_line(char *line)
@@ -62,24 +111,23 @@ char **lsh_split_line(char *line)
     return tokens;
 }
 
-int main()
+char *lsh_read_line(void)
 {
-    char *input;
+    char *line = NULL;
+    ssize_t bufsize = 0;
 
-    char *line;
-    char **args;
-    int status;
-
-    while (strcmp(line, "exit") != 0)
+    if (getline(&line, &bufsize, stdin) == -1)
     {
-        printf("> ");
-        line = lsh_read_line();
-        args = lsh_split_line(line);
-        printf("Comando executado: %s\n", line);
+        if (feof(stdin))
+        {
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            perror("readline");
+            exit(EXIT_FAILURE);
+        }
     }
 
-    printf("\n\n%s\n", line);
-
-    printf("Hello World!");
-    return 0;
+    return line;
 }
